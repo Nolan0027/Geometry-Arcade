a2fba3b4bf908ca156ef41574cafd5f45f4b1ffc
Nolan0027 <128444566+Nolan0027@users.noreply.github.com>
2025-01-17 03:54:03 -0600

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,63 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "Q2Zmq05oR0Sr]K%:-dMn":
+            case "Player":return img`
+f f f f f f f f f f f f f f f f 
+f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f 
+f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f 
+f 7 7 f f f f f f f f f f 7 7 f 
+f 7 7 f . . . . . . . . f 7 7 f 
+f 7 7 f . f f f f f f . f 7 7 f 
+f 7 7 f . f 9 9 9 9 f . f 7 7 f 
+f 7 7 f . f 9 9 9 9 f . f 7 7 f 
+f 7 7 f . f 9 9 9 9 f . f 7 7 f 
+f 7 7 f . f 9 9 9 9 f . f 7 7 f 
+f 7 7 f . f f f f f f . f 7 7 f 
+f 7 7 f . . . . . . . . f 7 7 f 
+f 7 7 f f f f f f f f f f 7 7 f 
+f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f 
+f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f 
+f f f f f f f f f f f f f f f f 
+`;
+            case "i#~e3(y_3":
+            case "PlayerDie":return img`
+. . . f f f f f . 1 . . . . . . 
+. . f 7 7 7 7 f 1 . 1 . . . 1 . 
+. f 7 7 7 7 7 7 1 1 6 1 1 . . . 
+. f 7 7 7 7 7 9 1 9 9 1 1 . 1 . 
+1 f 7 7 6 6 1 6 6 6 1 1 6 . . . 
+1 f 9 6 6 9 9 9 9 9 6 6 1 6 7 7 
+. 6 1 6 9 6 1 1 6 1 9 6 9 7 7 7 
+. 1 1 6 1 6 9 9 9 6 9 7 7 7 7 7 
+1 6 1 1 9 1 9 1 1 1 7 7 7 7 7 7 
+1 6 1 6 9 1 1 1 9 7 7 7 7 7 7 f 
+1 6 7 7 7 6 6 6 6 f f 7 7 7 f 1 
+1 1 f 7 7 7 7 7 7 7 6 f f f 1 . 
+1 1 6 f 7 7 7 7 7 7 f 9 1 . 1 1 
+. 1 . 6 f 7 7 f f f 1 1 1 . 1 . 
+. 1 1 . 6 f f 6 6 6 1 1 1 1 . . 
+. . . 1 . . 1 1 1 . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,56 @@
+scene.onHitWall(SpriteKind.Player, function (sprite, location) {
+    if (tiles.tileAtLocationEquals(location, assets.tile`Spike`) || tiles.tileAtLocationEquals(location, assets.tile`Spike0`) || (tiles.tileAtLocationEquals(location, assets.tile`Spike2`) || tiles.tileAtLocationEquals(location, assets.tile`Spike3`))) {
+        A = 1
+        Playar.vx = 0
+        music.play(music.melodyPlayable(music.powerDown), music.PlaybackMode.UntilDone)
+        Playar.vy = 100
+        Playar.setPosition(10, 110)
+        Progress.value = 0
+        Playar.vx = 100
+        A = 0
+    }
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    while (controller.A.isPressed()) {
+        Playar.vy = -105
+        timer.background(function () {
+            for (let index = 0; index < 6; index++) {
+                transformSprites.changeRotation(Playar, 30)
+                pause(150)
+                if (Playar.tileKindAt(TileDirection.Bottom, assets.tile`Block`) || (Playar.tileKindAt(TileDirection.Bottom, assets.tile`Slab`) || Playar.tileKindAt(TileDirection.Bottom, assets.tile`Block2`))) {
+                    transformSprites.rotateSprite(Playar, 0)
+                    break;
+                }
+            }
+        })
+        pause(400)
+        Playar.vy = 105
+        pause(400)
+    }
+})
+statusbars.onStatusReached(StatusBarKind.Energy, statusbars.StatusComparison.GTE, statusbars.ComparisonType.Percentage, 100, function (status) {
+    game.gameOver(true)
+    info.setScore(100)
+})
+let A = 0
+let Progress: StatusBarSprite = null
+let Playar: Sprite = null
+Playar = sprites.create(assets.image`Player`, SpriteKind.Player)
+scene.setBackgroundColor(9)
+tiles.setCurrentTilemap(tilemap`Level1`)
+Playar.setPosition(10, 110)
+Playar.vx = 100
+Playar.vy = 100
+Playar.startEffect(effects.trail)
+Progress = statusbars.create(70, 8, StatusBarKind.Energy)
+Progress.setColor(7, 0)
+Progress.value = 0
+Progress.setPosition(80, 17)
+A = 0
+cameraOffsetScene.cameraFollowWithOffset(Playar, 40, 0)
+forever(function () {
+    while (A == 0) {
+        Progress.value += 1
+        pause(tileUtil.tilemapProperty(tileUtil.currentTilemap(), tileUtil.TilemapProperty.PixelWidth) / 10)
+    }
+})
