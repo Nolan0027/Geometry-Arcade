416dbbacf40d2a449348761ef5fe93b9b3530b95
Nolan0027 <128444566+Nolan0027@users.noreply.github.com>
2025-01-18 14:50:53 -0600

    Added menu, tweaks, optimizations

--- images.g.ts
+++ images.g.ts
@@ -41,6 +41,106 @@ f 7 7 7 7 f . . . . f 7 7 7 f .
 . . f f 7 7 7 7 7 7 7 f f . . . 
 . . . . f f f f f f f f . . . . 
 `;
+            case "image1":
+            case "Jump0":return img`
+. . . . f f f f f f f f . . . . 
+. . f f 7 7 7 7 7 7 7 f f . . . 
+. f 7 7 7 7 7 f f 7 7 7 7 f . . 
+f 7 7 7 7 f . . . . f 7 7 7 f . 
+f 7 7 f f . . . . . . f 7 7 f . 
+f 7 f f . f f f f f . . f 7 7 . 
+f 7 f . . f 9 9 9 9 f . f 7 7 f 
+f f . . . f 9 9 9 9 f . f 7 7 f 
+f f . . . f 9 9 9 9 f . f 7 7 f 
+f 7 f . . f 9 9 9 f f . f 7 7 f 
+f 7 f f . f f f f f . . f 7 7 f 
+f 7 7 7 f . . . . . . f 7 7 7 f 
+f f 7 7 7 f . . . . f 7 7 7 f f 
+. f f 7 7 f f f f f 7 7 f f . . 
+. . f f 7 7 7 7 7 7 7 f . . . . 
+. . . f f f f f f f f . . . . . 
+`;
+            case "tEpsbQu?ZI@KkdZH[tPn":
+            case "Misc":return img`
+f f f f f 7 7 7 7 7 f f f f f 
+f 9 9 9 9 f 7 7 7 7 9 9 9 9 f 
+f 9 9 9 f 5 f 7 7 7 9 9 9 9 f 
+f 9 9 f 5 5 5 f 7 f 9 9 9 9 f 
+7 f f 5 5 f 5 f f 5 f 7 7 7 7 
+f 5 5 5 5 f f 7 f 5 5 f 7 7 7 
+f 5 5 f f 5 f f 5 f f 7 7 7 7 
+7 f f 7 7 f 5 5 f 7 7 7 7 7 7 
+7 7 7 7 7 f 5 5 f 7 7 7 7 7 7 
+7 7 7 f f 5 f f 5 f 7 7 7 7 7 
+7 7 f 5 5 f 7 7 f 5 f 7 7 7 7 
+f 9 9 f 5 f 7 7 7 f 5 f 9 9 f 
+f 9 9 9 f 7 7 7 7 7 f 9 9 9 f 
+f 9 9 9 9 7 7 7 7 7 9 9 9 9 f 
+f f f f f 7 7 7 7 7 f f f f f 
+`;
+            case "image3":
+            case "Title":return img`
+.1111111.......1111111111111f111111111111.....11111.....1111111.....11111111111111111111111111.1111111111.....11111....................1111111111111111....1111111111....11111111111.11111..11111...
+1cffffff1.....1cfffffffffffd1ffffffffffff1...1ffffb....1fffffff1...1fffffffffffffffffffffffffc1bfffffffff1..1dffff1....................dffffffffffffffb...1cffffffffc1..1bfffffffffd1cfffb..dffff1..
+1f7ddddcd...111cddddddddddfbbfddddddddddf1b11dfddfbbd111fd5557fdb111ed777777777f7777777777777cbbd7777777fdb11de77fd..11111.............bf577777777777fbbd11cd7777777fdb11ce7777777fddf777bbdbf77edb1
+ff77777fd...1fffd777777777ffffd777777777fffcfffd7fffbdfff77777fffcffc7777777777f7777777777777fffd7777777ffffffe77fd.ccfffb1............bf777777777777fffcfffd7777777fffcffe7777777fddf777ffcbf77fffb
+77777eefd..1cf77d77eeeeeeefe77d7eee7777777efe777777efbe777777777efb7777eeeeeeeefcc777777eeeeef77777ee77777ef7e777fd.cc7eefb............dfcc77777eee777eeffee777ee777eeffee777eeefefddf777eefbf777eec
+77777bddddddcc77777fdddd1ddb7777ecf7777777efd7777777fbd7777777777fd7777fbdddd1dddde77777cddd1c77777fce77777fd7777fbddcd77fb............11dbd7777fbf77777fc77777fcd7777ff57777fbddddddf77777fbf77777c
+77777bfffffffc77777ffffffbdb7777ecf77777777fd7777777ffd7777777777fd7777ffffffd...de77777cc..dc77777ffe77777fd7777ffffcd77fb..............cbd7777fbf77777fc77777ffd7777ff57777fffffffff77777fbf77777c
+77777bf77777ee7777777777fddb7777ecbbcb77777fd777777777777fce77777fd777777777eb...de77777cc..1c7777777777777f7777777efcd77fb..............bbd7777fbf77777fc777777777777ff777777777777ef77777fbf77777c
+77777bf77777ee7777777777fddb7777eccfd77777efd777777777777fbe77777fd777777777fb...de77777cc..1c7777777777777f7777777effd77fb..............1b77777fbf77777fc777777777777ff777777777777ef77777fff77777c
+77777bf77777ff77777fffffcbd77777fccfd777777f77777fff77777fbe77777f77777ffffffb...df77777cc..1f7777777777ffffffc777777777efb..............1b77777fbf77777ff777777777777ff777777777777ef7777777777777c
+77777cf77777ff77777fdbbb1cde7777ffbbc77777ef77777fbf77777fbe77777f77777fbbbbbb...df77777cc..1f7777777777fbbdddf777777777efb..............1b77777fbf77777ff777777777777ff777777777777ef7777777777777c
+77777ff77777ff77777fcffffddeee77effff77777ef77777fbfcc777fbe77777f77777fbffffb...df77777cc..1f77777ee777fffbfdfcccc77777efb..............1b77777fbf77777ff77777ee77777fffffffff77777ef777777ee77777c
+777777777777ff77777fc777fddddf777777777777ef77777fbddb777fbe77777f77777fbf77fb...df77777cc..1f77777fcf77777b.dddddde7777efb..............1b77777fbf77777ff77777fc77777fbddddddbe7777ffc7777fbf77777c
+777777777777ff77777ff777fd..bf777777777777ff777eefb..beeefbe77777f77777fff77fb...df77777cc..1f77777fbfc7777b.dfffffc7777efb..............1b77777fff777eeff77777fccc777fb...bcffc7777ffcc777fbfcc777c
+ff77777fff77ffff77777777fd..dcff77777777ffff777fffb..dffffbe77777ffff7777777fb...dfffc77cc..1f77777fbfff777b.de777777777efb..............1b7777777777fffff77777fbfff77fb...bf77777fffffff77fdfff777c
+dfc7777fbf77fdbc7777777efd..11bde7777777f1bc777fdcc..1dcccbe7777ebcdf7777777fb...d1bfc7ecc..1f77777fdcdf77cb.df777777777efb..............1bc777777777fbbcf77777fdcdf77fb...df77777fdcccbf77fdcdfc77c
+dffccccfbf77fdcccccccccffb.....dfcccccccf1.beeffd.........be77eefb.dfcccfcfcfb.....bffcfcc..1ffc777fd.dffcfb.dffc77777ffffb..............1bffe77777fffb.bfff777fb.dffffd...dfffffffd...bffffd.bffffc
+.1111111bf77fd.d111111111......1111111111..d1111..........bf77f11b.c111111111b......1111d...111bc7cfd..1111b.b11c77777fd11b...............111f77777f11b.b11c77cfb.11111b...11111111c...11111...1111d
+........bfccfd............................................bfccfd...............................bcccfd..........cccccccfd....................bfcccccfd.....bcccffb...................................
+`;
+            case "image4":
+            case "Info":return img`
+f f f f f 7 7 7 7 7 f f f f f 
+f 9 9 9 9 7 7 7 7 7 9 9 9 9 f 
+f 9 9 9 9 7 7 7 7 7 9 9 9 9 f 
+f 9 9 9 9 7 f f f 7 9 9 9 9 f 
+7 7 7 7 7 7 f 5 f 7 7 7 7 7 7 
+7 7 7 7 7 7 f f f 7 7 7 7 7 7 
+7 7 7 7 7 7 f 5 f 7 7 7 7 7 7 
+7 7 7 7 7 7 f 5 f 7 7 7 7 7 7 
+7 7 7 7 7 7 f 5 f 7 7 7 7 7 7 
+7 7 7 7 7 7 f 5 f 7 7 7 7 7 7 
+7 7 7 7 7 7 f 5 f 7 7 7 7 7 7 
+f 9 9 9 9 7 f 5 f 7 9 9 9 9 f 
+f 9 9 9 9 7 f f f 7 9 9 9 9 f 
+f 9 9 9 9 7 7 7 7 7 9 9 9 9 f 
+f f f f f 7 7 7 7 7 f f f f f 
+`;
+            case "#Wl_sW6]aZ$$BsoDr8[~":
+            case "Play":return img`
+fffffff777777fffffff
+f999999777777999999f
+f99999f777777999999f
+f9999f5f77777999999f
+f9999f55ff777999999f
+f9999f5555ff7999999f
+77777f555555ff777777
+77777f55555555ff7777
+77777f5555555555f777
+77777f55555555555f77
+77777f55555555555f77
+77777f5555555555f777
+77777f55555555ff7777
+77777f555555ff777777
+f9999f5555ff7999999f
+f9999f55ff777999999f
+f9999f5f77777999999f
+f99999f777777999999f
+f999999777777999999f
+fffffff777777fffffff
+`;
         }
         return null;
     })
--- main.ts
+++ main.ts
@@ -1,35 +1,76 @@
 scene.onHitWall(SpriteKind.Player, function (sprite, location) {
-    if (tiles.tileAtLocationEquals(location, assets.tile`Spike`) || tiles.tileAtLocationEquals(location, assets.tile`Spike0`) || (tiles.tileAtLocationEquals(location, assets.tile`Spike2`) || tiles.tileAtLocationEquals(location, assets.tile`Spike3`) || Playar.isHittingTile(CollisionDirection.Right))) {
+    if (InLevel == 1 && (tiles.tileAtLocationEquals(location, assets.tile`Spike3`) || (tiles.tileAtLocationEquals(location, assets.tile`Spike4`) || (tiles.tileAtLocationEquals(location, assets.tile`Spike0`) || tiles.tileAtLocationEquals(location, assets.tile`Spike2`))) || Playar.isHittingTile(CollisionDirection.Bottom))) {
         music.play(music.melodyPlayable(music.powerDown), music.PlaybackMode.UntilDone)
-        game.reset()
+        Progress.value = 0
+        tiles.placeOnTile(Playar, tiles.getTileLocation(0, 14))
+        Playar.vx = 110
     }
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    while (controller.A.isPressed()) {
+    while (controller.A.isPressed() && InLevel == 1) {
+        Playar.vy = -150
         Playar.setImage(assets.image`Jump`)
-        Playar.vy = -130
-        pause(360)
+        pause(370)
+        Playar.setImage(assets.image`Jump0`)
+        Playar.vy = 150
+        pause(370)
         Playar.setImage(assets.image`Player`)
-        Playar.vy = 110
-        pause(450)
     }
 })
+function Menu2 () {
+    scene.setBackgroundColor(9)
+    Title = sprites.create(assets.image`Title`, SpriteKind.Food)
+    Title.setPosition(81, 32)
+    Title.scale = 0.8
+    tiles.setCurrentTilemap(tilemap`MenuScreen`)
+    Menu = miniMenu.createMenu(
+    miniMenu.createMenuItem("1", assets.image`Info`),
+    miniMenu.createMenuItem("2", assets.image`Play`),
+    miniMenu.createMenuItem("3", assets.image`Misc`)
+    )
+    Menu.setStyleProperty(miniMenu.StyleKind.Default, miniMenu.StyleProperty.IconOnly, 1)
+    Menu.setMenuStyleProperty(miniMenu.MenuStyleProperty.Rows, 1)
+    Menu.setMenuStyleProperty(miniMenu.MenuStyleProperty.Columns, 3)
+    Menu.setStyleProperty(miniMenu.StyleKind.Default, miniMenu.StyleProperty.Alignment, 1)
+    Menu.setMenuStyleProperty(miniMenu.MenuStyleProperty.BackgroundColor, 0)
+    Menu.setDimensions(100, 50)
+    Menu.setPosition(77, 75)
+    Menu.onButtonPressed(controller.A, function (selection, selectedIndex) {
+        if (selectedIndex == 1) {
+            Menu.close()
+            Level(1)
+        }
+    })
+}
+function Level (Id: number) {
+    sprites.destroyAllSpritesOfKind(SpriteKind.Player)
+    if (Id == 1) {
+        tiles.setCurrentTilemap(tilemap`Level1`)
+    }
+    Playar = sprites.create(assets.image`Player`, SpriteKind.Player)
+    Progress = statusbars.create(90, 8, StatusBarKind.Energy)
+    Progress.setColor(7, 0)
+    Progress.value = 0
+    Progress.setPosition(80, 17)
+    Playar.vx = 110
+    tiles.placeOnTile(Playar, tiles.getTileLocation(0, 14))
+    cameraOffsetScene.cameraFollowWithOffset(Playar, 40, 0)
+    Playar.vy = 150
+    InLevel = 1
+}
 statusbars.onStatusReached(StatusBarKind.Energy, statusbars.StatusComparison.GTE, statusbars.ComparisonType.Percentage, 100, function (status) {
     game.gameOver(true)
 })
+let Menu: miniMenu.MenuSprite = null
+let Title: Sprite = null
+let Progress: StatusBarSprite = null
 let Playar: Sprite = null
-Playar = sprites.create(assets.image`Player`, SpriteKind.Player)
-scene.setBackgroundColor(9)
-tiles.setCurrentTilemap(tilemap`Level1`)
-Playar.setPosition(10, 120)
-Playar.vy = 100
-let Progress = statusbars.create(90, 8, StatusBarKind.Energy)
-Progress.setColor(7, 0)
-Progress.value = 0
-Progress.setPosition(80, 17)
-Playar.vx = 110
-cameraOffsetScene.cameraFollowWithOffset(Playar, 40, 0)
+let InLevel = 0
+InLevel = 0
+Menu2()
 forever(function () {
-    Progress.value += 1
-    pause(tileUtil.tilemapProperty(tileUtil.currentTilemap(), tileUtil.TilemapProperty.PixelWidth) / 10)
+    if (InLevel == 1) {
+        pause(tileUtil.tilemapProperty(tileUtil.currentTilemap(), tileUtil.TilemapProperty.PixelWidth) / 10)
+        Progress.value += 1
+    }
 })
